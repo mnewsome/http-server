c751bcbedf9f0529454e42123dcdba50bae6177c
mnewsome <malcolm.newsome@gmail.com>
2014-02-05 01:01:20 -0600

    moved a mock

--- src/com/httpserver/MockMethodNotAllowed.java
+++
@@ -1,8 +0,0 @@
-package com.httpserver;
-
-public class MockMethodNotAllowed {
-    public String generate(String requestURI) {
-        String response = "HTTP/1.1 405 Method Not Allowed\r\n";
-        return response;
-    }
-}
--- test/com/httpserver/MethodNotAllowedResponseTest.java
+++ test/com/httpserver/MethodNotAllowedResponseTest.java
@@ -11,4 +11,11 @@ public class MethodNotAllowedResponseTest {
         String response = new MethodNotAllowedResponse().generate("/test-file.txt");
         assertEquals("Method not allowed", testResponse, response);
     }
+
+    private class MockMethodNotAllowed {
+        public String generate(String requestURI) {
+            String response = "HTTP/1.1 405 Method Not Allowed\r\n";
+            return response;
+        }
+    }
 }
